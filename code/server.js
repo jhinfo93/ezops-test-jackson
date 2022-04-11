a7641dfdfee5d06f3d15afc80b475ce55be45e24
Jackson Henrique <jhsilva7@stefanini.com>
2022-04-11 17:49:09 -0300

    adicionando mongodb no projeto

--- code/server.js
+++ code/server.js
@@ -14,7 +14,8 @@ var Message = mongoose.model('Message',{
   message : String
 })
 // var dbUrl = "mongodb://simplechat:simplechat@mongo:27017/myFirstDatabase";
-var dbUrl = "mongodb://mongo:27017/simplechat";
+// var dbUrl = "mongodb://mongo:27017/simplechat";
+var dbUrl = "mongodb://localhost:27017/myFirstDatabase";
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
     res.send(messages);
